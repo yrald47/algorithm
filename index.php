7f8834ef72af31bb04298ad3b269d1b6e7436e6b
RALD <yr.alditya@gmail.com>
2019-02-16 12:56:59 +0700

    header location

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  	</head>
   	<body>
   		<?php
-  		// header('Location:http://justalgorithm.herokuapp.com/terbilang');
-  		header('Location:http://localhost/algorithm/terbilang');
+  		header('Location:http://justalgorithm.herokuapp.com/terbilang');
+  		// header('Location:http://localhost/algorithm/terbilang');
   		?>
 	</body>
 </html>
